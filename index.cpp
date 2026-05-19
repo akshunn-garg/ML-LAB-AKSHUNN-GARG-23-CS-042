c4dae1f1f871a485708f0dbbba72c5c5658e1796
Akshunn Garg <akshunn.garg2005@gmail.com>
2026-05-19 11:51:23 +0530

    all

--- index.cpp
+++ index.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 #include<string>
 using namespace std;
+void newfunc(){
+    cout<<"new feature"<<endl;
+    cout<<"new feature 2"<<endl;
+}
 int main(){
     int x=3;
     char c='a';
     string name="akshunn";
+    newfunc();
     return 0;
 }
